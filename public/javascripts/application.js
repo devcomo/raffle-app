02326aa3c8ea80a65dbc3a8f0f5749d73d350030
James Carr <james.r.carr@gmail.com>
2011-07-27 19:02:10 -0500

    here it is

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,16 +1,65 @@
 (function() {
-  var Tweet, Tweets, tweets;
-  Tweet = Backbone.Model.extend({});
-  Tweets = Backbone.Collection.extend({
-    model: Tweet,
-    url: 'http://search.twitter.com/search.json?q=comorichweb&callback=?',
-    parse: function(response) {
-      return response.results;
-    }
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  $(function() {
+    var Tweet, TweetView, Tweets, TweetsView;
+    Tweet = Backbone.Model.extend({});
+    Tweets = Backbone.Collection.extend({
+      model: Tweet,
+      url: 'http://search.twitter.com/search.json?q=comorichweb&callback=?&rpp=20',
+      parse: function(response) {
+        var results;
+        results = response.results;
+        return _.values(_.reduce(results, function(a, b) {
+          a[b.from_user_id] = b;
+          return a;
+        }, {}));
+      },
+      selectRandom: function() {
+        var toReturn;
+        toReturn = this.at(this.randomUpTo(this.length));
+        this.remove(toReturn);
+        return toReturn;
+      },
+      randomUpTo: function(ceiling) {
+        return Math.floor(Math.random() * ceiling);
+      }
+    });
+    TweetView = Backbone.View.extend({
+      tagName: 'li',
+      template: _.template($('#tweet-template').html()),
+      initialize: function() {
+        return this.model.view = this;
+      },
+      render: function() {
+        $(this.el).html(this.template(this.model.toJSON()));
+        return this;
+      }
+    });
+    TweetsView = Backbone.View.extend({
+      el: $('#tweets'),
+      events: {
+        'click': 'pickRandom'
+      },
+      initialize: function() {
+        this.tweets = new Tweets();
+        this.tweets.bind('reset', __bind(function(tweets) {
+          return tweets.each(__bind(function(tweet) {
+            return this.render(new TweetView({
+              model: tweet
+            }).render().el);
+          }, this));
+        }, this));
+        return this.tweets.fetch();
+      },
+      pickRandom: function() {
+        var tweet;
+        tweet = this.tweets.selectRandom();
+        return alert(tweet.get('from_user'));
+      },
+      render: function(tweet) {
+        return $(this.el).append(tweet);
+      }
+    });
+    return window.App = new TweetsView;
   });
-  tweets = new Tweets();
-  tweets.bind('reset', function(collection) {
-    return console.log(collection);
-  });
-  tweets.fetch();
 }).call(this);
--- views/javascripts/application.coffee
+++ views/javascripts/application.coffee
@@ -1,20 +1,48 @@
-Tweet = Backbone.Model.extend({});
+$ ->
+  Tweet = Backbone.Model.extend({});
 
-Tweets = Backbone.Collection.extend
-  model: Tweet
-  url: 'http://search.twitter.com/search.json?q=comorichweb&callback=?'
-  parse: (response)->
-    response.results
+  Tweets = Backbone.Collection.extend
+    model: Tweet
+    url: 'http://search.twitter.com/search.json?q=comorichweb&callback=?&rpp=20'
+    parse: (response)->
+      results = response.results
+      _.values _.reduce(results, (a, b) ->
+        a[b.from_user_id] = b
+        a
+      ,{})
+    selectRandom: -> 
+      toReturn = @at(@randomUpTo(@length))
+      @remove toReturn
+      toReturn
 
-tweets = new Tweets();
+    randomUpTo: (ceiling) ->
+      Math.floor(Math.random()*ceiling)
 
-tweets.bind 'reset', (collection) ->
-  console.log collection
+  TweetView = Backbone.View.extend
+    tagName:'li'
+    template: _.template($('#tweet-template').html())
+    initialize: ->
+      @model.view = @
+    render: ->
+      $(@el).html(@template(@model.toJSON()))
+      @
 
-tweets.fetch()
-
-TweetView = Backbone.View.extend
-  tagName:'li'
-  render: ->
-    $(@el).html(@template(@model.toJSON()))
-    
+  TweetsView = Backbone.View.extend
+    el:$('#tweets')
+    events: 
+      'click':'pickRandom'
+    initialize: ->
+      @tweets = new Tweets();
+      @tweets.bind 'reset', (tweets) =>
+        tweets.each (tweet) =>
+          @render(new TweetView(model:tweet).render().el)
+      @tweets.fetch()
+    pickRandom: ->
+      tweet = @tweets.selectRandom()
+      alert tweet.get('from_user')
+    render: (tweet) ->
+      $(@el).append tweet
+      
+  window.App = new TweetsView
+        
+        
